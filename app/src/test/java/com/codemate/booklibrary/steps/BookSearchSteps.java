3bc8ce5fc9d5160d63fec70c0e6c5b2b34efd708
Mischenko Sergey <sm@bamboogroup.eu>
2019-02-25 17:12:53 +0300

    Migrate to io.cucumber 4.2.3

--- app/src/test/java/com/codemate/booklibrary/steps/BookSearchSteps.java
+++ app/src/test/java/com/codemate/booklibrary/steps/BookSearchSteps.java
@@ -2,18 +2,11 @@ package com.codemate.booklibrary.steps;
 
 import com.codemate.booklibrary.data.Book;
 import com.codemate.booklibrary.data.Library;
-import com.codemate.booklibrary.ui.MainPresenter;
-import com.codemate.booklibrary.ui.MainView;
 
-import org.junit.Assert;
-import org.mockito.Mockito;
-
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import cucumber.api.Format;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -22,8 +15,6 @@ import cucumber.api.java.en.When;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class BookSearchSteps {
     private Library library = new Library();
@@ -33,8 +24,9 @@ public class BookSearchSteps {
      * Test data populations
      */
     @Given(".*a book with title \"([^\"]*)\", written by \"([^\"]*)\", published in (.+)$")
-    public void addNewBook(String title, String author, @Format("dd MMMM yyyy") Date publishDate) throws Throwable {
-        Book book = new Book(title, author, publishDate);
+    public void addNewBook(String title, String author, String publishDate) throws Throwable {
+        SimpleDateFormat df = new SimpleDateFormat("dd MMMM yyyy");
+        Book book = new Book(title, author, df.parse(publishDate));
         library.addBook(book);
     }
 
